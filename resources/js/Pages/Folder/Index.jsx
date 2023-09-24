7236a6bf5bb21145269752dd715ed86700e0d5ef
kaitokosuge <kaitokosuge.mi@gmail.com>
2023-09-24 10:07:50 +0900

    デザイン方向性が固まった

--- resources/js/Pages/Folder/Index.jsx
+++ resources/js/Pages/Folder/Index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from '@inertiajs/react';
+import Show from './Show';
 
 function Index(props) {
     console.log('index props',props);
@@ -9,11 +10,14 @@ function Index(props) {
         <>
             <ul>
                 {folders.map((folder) => (
-                    <li>
-                        <Link href={`/folder/${folder.id}`} className="flex items-center">
-                            <img className="w-[100px]"src={folder.image}/>
-                            <p>{folder.title}</p>
-                        </Link>
+                    <li className="flex">
+                        <div>
+                            <Link href={`/folder/${folder.id}`} className="flex items-center">
+                                <img className="w-[30px] rounded-md mr-1"src={folder.image}/>
+                                <p className="font-bold text-xs">{folder.title}</p>
+                            </Link>
+                        </div>
+                        <Show folder={folder}/>
                     </li>   
                 ))}
             </ul>
--- resources/js/Pages/Folder/Show.jsx
+++ resources/js/Pages/Folder/Show.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 
-function Show(props) {
-    console.log('show props',props);
-    const { folder } = props;
+function Show(folder) {
+    console.log('show props',folder);
+    //const { folder } = folder;
+    console.log('show folder.articles',folder.folder.articles)
     return (
         <>
             <img src={folder.image}/>
             <p>{folder.title}</p>
             <ul>
-                {folder.articles.map((article) => (
+                {folder.folder.articles.map((article) => (
                     <>
-                        <li className="flex items-center">
-                            <img src={article.image}/>
-                            <p>{article.title}</p>
+                        <li className="mt-5">
+                            <a href={article.url} target="_blank" className="flex items-center">
+                                <img className="w-[30px] rounded-md mr-5"src={article.image}/>
+                                <p className="text-xs mr-5">{article.title}</p>
+                                <p className="font-bold text-blue-400 mr-5">{article.url}</p>
+                                <p className="text-xs font-weight mr-5">{article.memo}</p>
+                            </a>
                         </li> 
                     </> 
                 ))}
