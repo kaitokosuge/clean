ef87854314b1ce3b402c6a1641d5e53855067bd4
kaitokosuge <kaitokosuge.mi@gmail.com>
2023-09-25 13:06:08 +0900

    migration,seederにカラーコード追加完了

--- database/migrations/2023_09_23_073246_create_folders_table.php
+++ database/migrations/2023_09_23_073246_create_folders_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('image');
             $table->foreignId('user_id')->constrained();
+            $table->string('rgb');
             $table->softDeletes();
             $table->timestamps();
         });
