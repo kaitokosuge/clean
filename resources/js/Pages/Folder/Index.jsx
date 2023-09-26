c223d3c52f5885773b47edb693056b18c2ad9cf0
kaitokosuge <kaitokosuge.mi@gmail.com>
2023-09-26 13:31:02 +0900

    layoutモック完了

--- resources/js/Pages/Folder/Index.jsx
+++ resources/js/Pages/Folder/Index.jsx
@@ -2,26 +2,29 @@ import React from "react";
 import { Link } from '@inertiajs/react';
 import Show from './Show';
 
+// indexはフォルダ一覧として使う
 function Index(props) {
     console.log('index props',props);
     const { folders } = props;
     console.log('index folders',folders);
     return (
-        <>
-            <ul>
-                {folders.map((folder) => (
-                    <li className="flex mt-10 p-5">
-                        <div className="w-[20%]">
-                            <Link href={`/folder/${folder.id}`} className="flex items-center">
+        <div>
+            <div class="bg-slate-900 w-screen h-screen fixed"></div>
+            <ul className="w-[50%]">
+                {folders.map((folder,index) => (
+                    <li className={index % 2 === 0 ? 'flex p-5 w-full relative z-10':'flex  p-5 w-full relative z-10 flex-row-reverse'}>
+                        <Link href={`/folder/${folder.id}`} className="block" >
+                            <div className="rounded-full shadow w-[200px] h-[200px]" style={{ backgroundColor: `rgb(${folder.rgb})`, }}></div>
+                            <div className="flex items-center mt-5">
                                 <img className="w-[30px] rounded-md mr-1"src={folder.image}/>
-                                <p className="font-bold text-xs">{folder.title}</p>
-                            </Link>
-                        </div>
-                        <Show folder={folder}/>
+                                <p className="font-bold text-xs text-gray-300">{folder.title}</p>
+                            </div>
+                            <time className="font-bold text-xs text-gray-500 block mt-1">{folder.updated_at}</time>
+                        </Link>
                     </li>   
                 ))}
             </ul>
-        </>
+        </div>
     );
 }
 
